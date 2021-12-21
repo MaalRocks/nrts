3577ea4b8c2128f1685f55dd30ab3fd16207fc9c
maalrocks <spammersohn@gmail.com>
2021-12-21 11:08:59 +0100

    sinnvolle commit beschreibung

--- components/GridGallery.js
+++ components/GridGallery.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from 'C:/Users/Lono/Desktop/React/nrts/styles/GridGallery.module.sass'
 import GalleryImage from '../components/GalleryImage';
 
 let imageArray = [
@@ -20,8 +21,8 @@ function GalleryImageFrame(props) {
 
 export default function GridGallery() {
     return (
-        <div className="grid-gallery-wrapper">
-            <div className="grid-gallery">
+        <div className={styles.wrapper}>
+            <div className={styles.gridGallery}>
                 {imageArray.map((galleryImage) => <GalleryImageFrame key={galleryImage} imageName={galleryImage} />)}
             </div>
 
